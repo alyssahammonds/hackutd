6b35cf5717f4bdf9f1c5fefb84228ff04a41831d
Lucas Ribeiro Vidal <lucas.ribeirovid@gmail.com>
2023-11-04 15:02:12 -0500

    initial setup backend

---
+++ backend/src/main/java/code/HelloWorld.java
@@ -0,0 +1,10 @@
+package code;
+import org.joda.time.LocalTime;
+
+// This file is just for testing
+public class HelloWorld {
+  public static void main(String[] args) {
+    LocalTime currentTime = new LocalTime();
+  System.out.println("it is currently " + currentTime);
+  }
+}
